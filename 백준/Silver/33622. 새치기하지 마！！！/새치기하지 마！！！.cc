e1ff22519967e2339067bcf62173b9c5989b7b05
2u6in <zxcv35602667@gmail.com>
2025-11-18 17:35:27 +0900

    [Silver V] Title: 새치기하지 마!!!, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/33622. 새치기하지 마！！！/새치기하지 마！！！.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using ll = long long;
+using pp = pair<int,int>;
+
+const ll INF = (ll)1e18;
+#define MAX 1e9
+const int MOD = 1000000007;
+
+int main(){
+    fastio;
+
+    int n;
+    cin>>n;
+    cout<<1<<'\n';
+    cout<<n;
+
+    return 0;
+}
