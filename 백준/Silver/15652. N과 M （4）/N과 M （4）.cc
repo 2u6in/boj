70dbca78f60af8fc4e12fa51b26def04986d1f60
2u6in <zxcv35602667@gmail.com>
2026-01-02 04:59:08 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int N,M;
+int arr[9];
+
+void dfs(int num, int k) {
+    if(k==M) {
+        for(auto i =0;i<M;i++)
+            cout << arr[i] << " ";
+        cout << "\n";
+    } else {
+        for(auto i=num; i<=N;i++) {
+                arr[k]=i;
+                dfs(i,k+1);
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    dfs(1,0);
+}
